e5ad8127baee8daab6aa0bca6967dc6ed09ce16d
Yuchen Gu <guyuchen999@gmail.com>
2025-06-17 15:56:19 -0700

    Added Posting Model

--- server/src/modules/posting/posting.model.js
+++ server/src/modules/posting/posting.model.js
@@ -1,5 +1,47 @@
 import mongoose from "mongoose";
 
-const PostingSchema = new mongoose.Schema({});
+const PostingSchema = new mongoose.Schema(
+  {
+    sellerId: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: [String],
+      default: [],
+    },
+    pickupLocation: {
+      type: String,
+      required: true,
+    },
+    viewedBy: {
+      type: [String],
+      default: [],
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 export const Posting = mongoose.model("Posting", PostingSchema);
